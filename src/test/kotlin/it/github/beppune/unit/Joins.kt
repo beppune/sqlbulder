e2aa04d6485384111da5bfca75c3c3c50bde5fe4
manzogi9 <manzogi9@posteitaliane.it>
2026-03-04 08:55:53 +0100

    initial commit

---
+++ src/test/kotlin/it/github/beppune/unit/Joins.kt
@@ -0,0 +1,16 @@
+package it.github.beppune.unit
+
+import io.github.beppune.sqlbuilder.select
+import kotlin.test.Test
+
+class Joins {
+
+    //@Test
+    fun join() {
+
+        val joins = select()
+
+
+    }
+
+}
